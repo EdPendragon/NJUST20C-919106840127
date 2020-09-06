f292ff18721130a6d148819d91909932163c61d5
EdPendragon <70497817+EdPendragon@users.noreply.github.com>
2020-09-06 23:50:13 +0800

    Add files via upload

--- 919106840127个人项目.cpp
+++ 919106840127个人项目.cpp
@@ -1,193 +1,75 @@
-//!!!!!!!注意：目前小数减法仍然有很大的bug，负整数减法仍然有小瑕疵，9月6日不要忘记bug内容 
 #include <iostream>
 #include <stdio.h>
 #include <string>
 using namespace std;
-bool xiaoyu(string a,string b) { //该函数用于判断两字符串大小
+string tiqu(string str,int a,int b) { //提取字符串串中从a位到b位的字符，可用于去前放的正负号等功能
+	string ret="";
+	if(a>b) return ret;
+	for(int i=a; i<=b; i++) ret+=str[i];
+	return ret;
+}
+bool xiaoyu(string a,string b) {			//用于判断代表数字的字符串a是否小于b(无小数点，不考虑负号)
 	if(a.size()<b.size()) return true;
 	else if(a.size()>b.size()) return false;
-	else {
-		for(int i=0; i<a.size()-1; i++) {
+	else for(int i=0; i<a.size(); i++) {  //位数相同
 			if(a[i]<b[i]) return true;
-			else if(a[i]>b[i]) return false;
-			else continue;
+			else if(a[i]>b[i]) return false;  //按位对比
 		}
-		if(a[a.size()-1]>=b[b.size()-1]) return false;
-		if(a[a.size()-1]<b[b.size()-1]) return true;
-	}
+	return false;                        //说明二者相等，不小于，false
 }
-bool dengyu(string A,string B) { //该函数用于判断两字符串数字相等
-	string a=A,b=B;
-	if(a.size()!=b.size()) return false;
-	for(int i=0; i<a.size(); i++) {
-		if(a[i]!=b[i]) return false;
-	}
-	return true;
-}
-string quling(string str) {   //此函数用于去除字符串首0
-	int sf=0;		//首个非0的元素位置
-	if(str[0]=='-') sf=1;
-	for(int i=sf; i<str.size(); i++) {
-		if(str[i]!='0') {
-			sf=i;
+string clear(string A) {    				//去除字符串前边的空零，字符串末的多余零，将多余的负号转化为单个或正号
+	int dian=0,dianwei;
+	string a=A;
+	for(int i=0; i<a.size(); i++) {    //判断是否有小数点
+		if(a[i]=='.') {
+			dian=1;
+			dianwei=i;
 			break;
 		}
 	}
-	string jie="";
-	if(str[0]=='-') jie+="-";
-	for(int i=sf; i<str.size(); i++) jie+=str[i];
-	return jie;
-}
-string clear(string str) {   //此函数用于去除小数点后的尾0,并且将字符串前的--变成+
-	int youdian=0;	//看是否有小数点
-	for(int i=0; i<str.size(); i++) {
-		if(str[i]=='.') youdian=1;
+	if(dian) {  							//假如有小数点，去除末尾多余的零，假如末尾只有0，去除小数点
+		for(int i=a.size()-1; i>dianwei; i--) {
+			if(a[i]!='0') break;
+			if(a[i]=='0') a=tiqu(a,0,i-1);
+		}
+		if((a[dianwei+1]=='0'&&a.size()==dianwei+2)||(a.size()==dianwei+1)) {   //假如小数点之后只有一位0或只有小数点
+			a=tiqu(a,0,dianwei-1);
+		}
 	}
-	if(youdian==0) return str;
-	int biao=str.size()-1;
-	string jie="";
-	for(int i=str.size()-1; i>=0; i--) {
-		if(str[i]=='0') continue;
-		else {
-			biao=i;
+	int fuhao=0,fuhaowei;
+	if(a[0]=='-') {
+		fuhao=1,fuhaowei=0;             //fuhao记录负号数量，fuhaowei记录末位负号
+		for(int i=1; i<a.size(); i++) {
+			if(a[i]=='-') {
+				fuhao++;
+				fuhaowei=i;
+			} else break;
+		}
+		if(fuhao%2==0) a=tiqu(a,fuhaowei+1,a.size()-1);
+		else a=tiqu(a,fuhaowei,a.size()-1);
+	}
+	int sf=0,j;							//sf记录首个非0元素位置
+	if(a[0]=='-') j=1;
+	else j=0;
+	for(; j<a.size(); j++) {
+		if(a[j]!='0') {
+			sf=j;
 			break;
 		}
 	}
-	int j=0;
-	if(str[0]=='-'&&str[1]=='-') j=2;
-	if(biao==str.size()-1) {
-		for(; j<str.size(); j++) {
-			jie+=str[j];
-		}
-	} else {
-		for(; j<=biao; j++) {
-			jie+=str[j];
+	string tem1="",tem2="";
+	if(a[0]=='-') tem1+="-";             //tem1记录负号情况
+	tem2+=tiqu(a,sf,a.size()-1);		//tem2记录去除前方无用的0后的字符串
+	a=tem1+tem2;
+	int ql=1;							//判断是否全是0
+	for(int i=0;i<a.size();i++){
+		if(a[i]!='0'){
+			ql=0;
+			break;
 		}
 	}
-	return jie;
-}
-string jianfa(string A,string B) {
-	string a=A,b=B,a1="",a2="",b1="",b2="";  	//用a与b代替A与B进行运算，a1、a2为a的小数点前后两部分，b1、b2同理
-	a=clear(a);
-	b=clear(b);
-	a=quling(a);
-	b=quling(b);
-	if(dengyu(a,b)) return "0";
-	int diana,dianb,la1,la2,lb1,lb2; //diana,dianb用来记录a，b小数点位置，la1、la2记录a的小数点前和小数点后位置，lb1、lb2同理
-	int pa=0,pb=0;		//用于判断a，b是否有小数点
-	for(int i=0; i<a.size(); i++) if(a[i]=='.') {
-			diana=i;
-			pa=1;
-			break;		//记录a的小数点位置
-		}
-	for(int i=0; i<b.size(); i++) if(b[i]=='.') {
-			dianb=i;
-			pb=1;
-			break;		//记录b的小数点位置
-		}
-	if(pa==1&&pb==0) b=b+".0";
-	else if(pa==0&&pb==1) a=a+".";		//假设只有一个字符串没有小数点则给它加上小数点0
-	else if(pa==0&&pb==0) {			//否则进行整数减法
-		string jieguo="";
-		if(xiaoyu(a,b)) {     //假如a小于b
-			string tem=a;
-			a=b;
-			b=tem;
-			jieguo="-"+jieguo;
-		}
-		int flag=0,la=a.size(),lb=b.size();  //flag记录进位情况，la，lb记录a、b的长度
-		while(la>0) {
-			int na=a[la-1]-'0',nb;	//na保存a当前数字，nb保存b当前数字或者0
-			if(lb>0) nb=b[lb-1]-'0';
-			else nb=0;
-			if(na>=nb) {
-				a[la-1]='0'+na-nb;
-				flag=0;
-			} else {
-				a[la-1]='0'+na+10-nb;
-				flag=1;
-			}
-			if(flag) a[la-2]--;		//假如flag说明此次运算借位了，往前一位需要减一
-			la--;
-			lb--;					//准备进行下一位计算
-		}
-		jieguo+=a;
-		jieguo=clear(jieguo);
-		jieguo=quling(jieguo);
-		if(a[0]=='-'&&b[0]=='-') jieguo="-"+jieguo;
-		jieguo=clear(jieguo);
-		return jieguo;
-	}
-	for(int i=0; i<diana; i++) a1+=a[i];		//从a[0]到a[diana-1]记录为a1
-	for(int i=diana+1; i<a.size(); i++) a2+=a[i];		//从a[diana+1]到最后一位记录为a2
-	for(int i=0; i<dianb; i++) b1+=b[i];		//从b[0]到b[dianb-1]记录为b1
-	for(int i=dianb+1; i<b.size(); i++) b2+=b[i];		//从b[dianb+1]到最后一位记录为b2
-	string jieguo="";
-	if(xiaoyu(a1,b1)) { //假如a1小于b1
-		string tem=b1;
-		b1=a1;
-		a1=tem;
-		jieguo="-"+jieguo;
-		tem=b2;
-		b2=a2;
-		a2=tem;
-	}
-	int flag=0,xiaohou,flag_=0;   //小后代表小数点后的位数，即两个字符串小数部分长的那一个
-	la2=a2.size(),lb2=b2.size();//flag记录进位情况，la1,lb1记录a1、b1的长度,flag_判断小数点前第一位是否需要为小数点后第一位借位
-	if(la2>lb2) xiaohou=la2;
-	else xiaohou=lb2;
-	while(xiaohou>0) {
-		int na2,nb2,jiana=0,jianb=0;	//判断是否要la2，lb2--
-		if(xiaohou==la2) {		//假如当前位置可减
-			na2=a2[la2-1]-'0';
-			jiana=1;
-		} else na2=0;
-		if(xiaohou==lb2) {		//假如当前位置可减
-			nb2=b2[lb2-1]-'0';
-			jianb=1;
-		} else nb2=0;
-		if(la1==1) {				//考虑小数点后第一位的情况
-			if(na2>nb2) {
-				a2[la2-1]='0'+na2-nb2;
-				flag_=0;
-			} else {
-				a2[la2-1]='0'+na2-nb2+10;
-				flag_=1;
-			}						//以上几行进行减法
-			if(flag_) a1[a1.size()-1]--;
-		} else {						//正常情况
-			if(na2>nb2) {
-				a2[la2-1]='0'+na2-nb2;
-				flag=0;
-			} else {
-				a[la2-1]='0'+na2+10-nb2;
-				flag=1;
-			}						//以上几行进行减法
-			if(flag) a2[la2-2]--;
-		}
-		xiaohou--;
-		if(jiana) la2--;
-		if(jianb) lb2--;			//假如有jiana，jianb，则--
-	}
-	flag=0;
-	la1=a1.size(),lb1=b1.size();	//flag记录进位情况，la1，lb1记录a1、b1的长度
-	while(la1>0) {
-		int na1=a1[la1-1]-'0',nb1;  	//na1保存a1当前数字，nb1保存b1当前数字或者0
-		if(lb1>0) nb1=b1[lb1-1]-'0';
-		else nb1=0;
-		if(na1>nb1) {
-			a1[la1-1]='0'+na1-nb1;
-			flag=0;
-		} else {
-			a1[la1-1]='0'+na1+10-nb1;
-			flag=1;
-		}
-		if(flag) a1[la1-2]--;		//假如flag说明此次运算借位了，往前一位需要减一
-		la1--;
-		lb1--;						//往前一位
-	}
-	jieguo=jieguo+a1+"."+a2;
-	return jieguo;
+	if(ql) a="0";
+	return a;
 }
 string jiafa(string A,string B) {
 	string a=A,b=B,a1="",a2="",b1="",b2="";     //用a与b代替A与B进行运算,a1、a2为a的小数点前后两部分，b1、b2同理
@@ -288,11 +170,58 @@ string jiafa(string A,string B) {
 	else jieguo=a1;						//假如结果是整数，则a2为0，小数点可以省略，直接返回a1
 	return jieguo;		//返回结果
 }
+string zhengshujian(string A,string B) {    //因减法的特殊性，将整数减法与小数减法分开编写
+	string jieguo="";
+	string a=clear(A),b=clear(B);
+	int la=a.size(),lb=b.size();
+	if(xiaoyu(a,b)) {						//假如是小数减大数，则转化为负的大减小 
+		jieguo+="-";
+		jieguo+=zhengshujian(b,a);
+		jieguo=clear(jieguo);
+		return jieguo;
+	}
+	if(a[0]=='-'&&b[0]!='-'){				//假如是负数减正数，则转化为负号加加法 
+		jieguo+="-";
+		jieguo+=jiafa(tiqu(a,1,a.size()-1),b);
+		jieguo=clear(jieguo);
+		return jieguo;
+	}
+	if(a[0]!='-'&&b[0]=='-'){				//假如是正数减负数，则转化为加法 
+		jieguo+=jiafa(tiqu(b,1,b.size()-1),a);
+		jieguo=clear(jieguo);
+		return jieguo;
+	}
+	if(a[0]=='-'&&b[0]=='-'){				//假如是负数减负数，则转化为后减前 
+		jieguo+=zhengshujian(tiqu(b,1,b.size()-1),tiqu(a,1,a.size()-1));
+		jieguo=clear(jieguo);
+		return jieguo;
+	}
+	int flag=0;							//flag代表借位
+	while(la>0) {
+		int na=a[la-1]-'0',nb;					//na,nb记录a、b当前位置的数字
+		if(lb>0) nb=b[lb-1]-'0';
+		else nb=0;
+		int sum=na-nb;
+		if(sum>=0) {
+			a[la-1]='0'+sum;
+			flag=0;
+		} else {
+			a[la-1]='0'+sum+10;
+			flag=1;								//flag=1说明要借位
+		}
+		if(flag) a[la-2]--;						//借位
+		la--;
+		lb--;
+	}
+	jieguo+=a;
+	jieguo=clear(jieguo);						//再次去首0 
+	return jieguo;
+}
 int main() {
 	string ceshi1,ceshi2,jieguo;
 	while(cin>>ceshi1>>ceshi2) {
 		if(ceshi1=="0"&&ceshi2=="0") break;
-		else jieguo=jianfa(ceshi1,ceshi2);
+		else jieguo=zhengshujian(ceshi1,ceshi2);
 		cout<<jieguo<<endl;
 	}
 	return 0;
